95a212d89f369200d1165f2080b501de7e4c5fbb
David Kelly <kellydavid178@gmail.com>
2019-11-30 18:11:28 +0000

    Added Server object

--- core/src/main/scala/zio/http/doc/openapi/OpenApi.scala
+++ core/src/main/scala/zio/http/doc/openapi/OpenApi.scala
@@ -1,17 +1,15 @@
 package zio.http.doc.openapi
 
-case class OpenApi(
-                    openapi: String = "3.0.2",
-                    info: Info,
-                    servers: List[Server],
-                    paths: List[Path],
-                    components: Option[Components],
-                    security: List[SecurityRequirement],
-                    tags: List[Tag],
-                    externalDocs: Option[ExternalDocumentation]
-                  )
+// MAY be extended with Specification Extensions
+case class OpenApi(openapi: String = "3.0.2",
+                   info: Info,
+                   servers: List[Server],
+                   paths: List[Path],
+                   components: Option[Components],
+                   security: List[SecurityRequirement],
+                   tags: List[Tag],
+                   externalDocs: Option[ExternalDocumentation])
 
-sealed trait Server
 sealed trait Path
 sealed trait Components
 sealed trait SecurityRequirement
@@ -23,16 +21,36 @@ object OpenApi {
   type Email = String
 }
 
-case class Info(
-                 title: String,
-                 description: Option[String],
-                 termsOfService: Option[String],
-                 contact: Option[Info.Contact],
-                 license: Option[Info.License],
-                 version: String
-               )
+// MAY be extended with Specification Extensions
+case class Info(title: String,
+                description: Option[String],
+                termsOfService: Option[String],
+                contact: Option[Info.Contact],
+                license: Option[Info.License],
+                version: String)
 
 object Info {
-  case class Contact(name: Option[String], url: Option[OpenApi.Url], email: Option[OpenApi.Email])
+
+  // MAY be extended with Specification Extensions
+  case class Contact(name: Option[String],
+                     url: Option[OpenApi.Url],
+                     email: Option[OpenApi.Email])
+
+  // MAY be extended with Specification Extensions
   case class License(name: String, url: Option[OpenApi.Url])
+
+}
+
+// MAY be extended with Specification Extensions
+case class Server(url: OpenApi.Url,
+                  description: Option[String],
+                  variables: Map[String, Server.ServerVariable])
+
+object Server {
+
+  // MAY be extended with Specification Extensions
+  case class ServerVariable(enum: List[String],
+                            default: String,
+                            description: Option[String])
+
 }
